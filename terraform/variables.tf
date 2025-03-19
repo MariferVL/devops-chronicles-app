ef85d15e9abc6e8ef150c3eefc7da8c41a5ed33a
MariferVL <mfvl.1186@gmail.com>
2025-03-19 18:57:40 -0300

    Add db_host and db_root_pass

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,6 +4,11 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "db_host" {
+  description = "Database host"
+  type        = string
+}
+
 variable "db_user" {
   description = "Database user"
   type        = string
@@ -15,8 +20,19 @@ variable "db_pass" {
   sensitive   = true
 }
 
+variable "db_root_pass" {
+  description = "Database root password"
+  type        = string
+  sensitive   = true
+}
+
 variable "db_name" {
   description = "Database name"
+  type        = string
+}
+
+variable "flask_env" {
+  description = "Flask environment setting (e.g., production, development, staging)"
   type        = string
 }
 
